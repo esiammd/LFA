47efdb1be1d5f7ee8c83b2440c6b2da288469616
Maíse <esiammd@hotmail.com>
2018-06-17 11:10:56 -0300

    algoritmoAFD final

--- algoritmoAFD/algoritmoAFD.py
+++ algoritmoAFD/algoritmoAFD.py
@@ -1,47 +1,19 @@
-#entradas teste
-#chaves: i,a/i,b/a+,a/a+,b/b+,a/b+,b
-#estSegue: a+/b+/a+/b+/a+/b+
-#estFinais: b+
-#palavras: a/b/x/aab/babxb
+#formato entrada: estadoAtual, simboloLido, resultadoTransicao, ehFinal
+entrada=input()#entrada teste: 'i,a,a+,0/i,b,b+,1/a+,a,a+,0/a+,b,b+,1/b+,a,a+,0/b+,b,b+,1'
+tuplaEntrada=tuple(entrada.split('/'))
+transicao={} #chave da tabela hash
+for te in range(len(tuplaEntrada)):
+    tuplaTe = tuple(tuplaEntrada[te].split(','))
+    transicao[tuplaTe[0:2]]=tuplaTe[2:4]
 
-chaves=input('-> ')
-estSegue=input('-> ')
-estFinais=input('-> ')
-palavras=input('-> ')
+estFinal=0
 estAtual='i'
-
-tuplas1=tuple(chaves.split('/'))
-tuplasChaves=[]
-
-for t in range(len(tuplas1)):
-    tuplas2=tuple(tuplas1[t].split(','))
-    tuplasChaves.append(tuplas2)
-
-tuplasChaves=tuple(tuplasChaves)
-tuplasEstSegue=tuple(estSegue.split('/'))
-tuplasEstFinais=tuple(estFinais.split('/'))
-tuplasPalavras=tuple(palavras.split('/'))
-
-for tp in range(len(tuplasPalavras)):
-    for i in range(len(tuplasPalavras[tp])):
-        transicao = tuplasPalavras[tp][i]
-        erro = True
-        for j in range(len(tuplasChaves)):
-            if ((estAtual, transicao) == tuplasChaves[j]):
-                estAtual = tuplasEstSegue[j]
-                erro = False
-                break
-
-        if erro == True:
-            print('Nao pertence')
-            break
-
-    if erro == False:
-        ehFinal = False
-        for k in range(len(tuplasEstFinais)):
-            if estAtual==tuplasEstFinais[k]:
-                print('Pertence')
-                ehFinal = True
-                break
-        if ehFinal == False:
-            print('Nao pertence')
+palavra=input()#palavra teste: 'abba'
+for p in palavra:
+    try:
+        estFinal=transicao[estAtual,p][1]
+        estAtual=transicao[estAtual,p][0]
+    except KeyError:
+        estFinal = 0
+        break
+print(estFinal)
